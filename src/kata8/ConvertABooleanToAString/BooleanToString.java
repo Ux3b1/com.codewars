ad360ace38443e29a425ae6fafa659f01ec92964
Ux3b1 <posadrc@yandex.ru>
2021-02-28 14:53:08 +0300

    complete kata

---
+++ src/kata8/ConvertABooleanToAString/BooleanToString.java
@@ -0,0 +1,7 @@
+package kata8.ConvertABooleanToAString;
+
+public class BooleanToString {
+    public static String convert(boolean b) {
+        return b ? "true" : "false";
+    }
+}
---
+++ test/kata8/ConvertABooleanToAString/BoolToStrTest.java
@@ -0,0 +1,14 @@
+package kata8.ConvertABooleanToAString;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BoolToStrTest{
+    @Test public void testTrue(){
+        assertEquals(BooleanToString.convert(true), "true");
+    }
+
+    @Test public void testFalse(){
+        assertEquals(BooleanToString.convert(false), "false");
+    }
+}
